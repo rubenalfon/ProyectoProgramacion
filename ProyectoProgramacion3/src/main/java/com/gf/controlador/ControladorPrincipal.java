3933073e0d064d270f9f810671100268eb5d5c97
rubenalfon <118816291+rubenalfon@users.noreply.github.com>
2023-06-08 19:32:36 +0200

    Retoques

--- ProyectoProgramacion3/src/main/java/com/gf/controlador/ControladorPrincipal.java
+++ ProyectoProgramacion3/src/main/java/com/gf/controlador/ControladorPrincipal.java
@@ -60,27 +60,26 @@ public class ControladorPrincipal implements WindowListener, ActionListener {
         this.vista.setVisible(true);
         PantallaInfo.configPantalla(vista);
         PantallaInfo.setPosicion(vista);
-        
-        if(inJuego){//si esta en un juego y cierra la pestaña pues muestra el resultado
+
+        if (inJuego) {//si esta en un juego y cierra la pestaña pues muestra el resultado
             switch (juegoSeleccionado) {
                 case 0:
                     break;
                 case 1:
-                    panelMensaje( gregorio.getPuntuacion(), gregorio.getNumPreguntas());
+                    panelMensaje(gregorio.getPuntuacion(), gregorio.getNumPreguntas());
                     break;
                 case 2:
                     panelMensaje(mapa.getPuntuacion(), mapa.getNumPreguntas());
                     break;
                 case 3:
-                    panelMensaje( verdaderoFalso.getContadorAciertos(), verdaderoFalso.getNumPreguntas());
-                    break;                
+                    panelMensaje(verdaderoFalso.getContadorAciertos(), verdaderoFalso.getNumPreguntas());
+                    break;
                 case 4:
                     panelMensaje(quienLoHizo.getContadorAciertos(), quienLoHizo.getNumPreguntas());
                     break;
-
             }
-        }else{
-            this.inJuego=false;
+        } else {
+            this.inJuego = false;
         }
     }
 
@@ -102,54 +101,55 @@ public class ControladorPrincipal implements WindowListener, ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-        this.vista.setVisible(false);
-        switch (((JButton)e.getSource()).getName()) {//En funcion del boton que escoja este te creara un controlador 
+        switch (((JButton) e.getSource()).getName()) { // En función del boton que escoja este te creara un controlador 
             case "recorrido":
-                juegoSeleccionado=0;
-                recorrido= new ControladorRecorrido();
+                this.vista.setVisible(false);
+                juegoSeleccionado = 0;
+                recorrido = new ControladorRecorrido();
                 this.recorrido.addWindowListener(this);
                 break;
+
             case "gregorio":
-                juegoSeleccionado=1;
+                this.vista.setVisible(false);
+                juegoSeleccionado = 1;
                 gregorio = new ControladorGregorio(new VistaGregorio(), 5);
                 gregorio.getVista().addWindowListener(this);
                 break;
-            case "mapa":
-                juegoSeleccionado=2;
 
+            case "mapa":
+                this.vista.setVisible(false);
+                juegoSeleccionado = 2;
                 mapa = new ControladorMapa(new VistaMapa(), 6);
                 mapa.getVista().addWindowListener(this);
                 break;
+
             case "verdadero":
+                this.vista.setVisible(false);
                 this.vista.setVisible(false);
                 juegoSeleccionado = 3;
                 verdaderoFalso = new ControladorVerdaderoFalsoMuseos(new vistaVerdaderoFalsoMuseos(), 4);
                 verdaderoFalso.getVista().addWindowListener(this);
                 break;
+
             case "quien":
+                this.vista.setVisible(false);
                 this.vista.setVisible(false);
                 juegoSeleccionado = 4;
                 quienLoHizo = new ControladorVistaQuienLoHizo(new VistaQuienLoHizo(), 5, 5);
                 quienLoHizo.getVista().addWindowListener(this);
                 break;
+
             case "puntuaciones":
                 mostrarClasificacion();
                 break;
         }
-         this.inJuego=true;
+        this.inJuego = true;
     }
-    private void panelMensaje( int puntuacion, int numPreguntas){
-        JOptionPane.showMessageDialog(null, "¡Felicidades! Has acertado  " + puntuacion + " / " + numPreguntas);
-    }
-    
 
-
-    private void panelMensaje(JFrame vista, int puntuacion, int numPreguntas) {
-        System.out.println(puntuacion + "-" +  numPreguntas / 2);
+    private void panelMensaje(int puntuacion, int numPreguntas) {
+        System.out.println(puntuacion + "-" + numPreguntas / 2);
         if (puntuacion < numPreguntas / 2) {
             JOptionPane.showMessageDialog(vista, "¡Ánimos! Has acertado  " + puntuacion + " / " + numPreguntas);
-            
         } else {
             JOptionPane.showMessageDialog(vista, "¡Felicidades! Has acertado  " + puntuacion + " / " + numPreguntas);
         }
